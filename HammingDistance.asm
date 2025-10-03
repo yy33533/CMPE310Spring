945cb574c5899df5eb804cca1ea3e1a9608c04de
yy33533 <yy33533@umbc.edu>
2025-10-03 10:01:01 -0400

    Update HammingDistance.asm

--- HammingDistance.asm
+++ HammingDistance.asm
@@ -1,5 +1,36 @@
+section .data
+
+    DATA1: db "foo"
+
+    DATA2: db "bar"
+
+    LENGTH1: equ $-DATA1
+
+    LENGTH2: equ $-DATA2
+
+;section .bss
+
+
 global main
 
-section .text
+section .txt
 
-main:
+    main:
+
+
+        cmp LENGTH1, LENGTH2
+
+        ja 
+
+        jbe
+
+
+
+
+        SETFIRST:
+
+        mov cl, 
+
+        SETNEXT:
+
+
